f6a7969f54571c9ac1a9a7cb62b9a552c9219238
Nicolas Guarini <nikiguarincia@gmail.com>
2020-09-28 12:51:15 +0200

    OOP refactor

--- P754Converter.java
+++ P754Converter.java
@@ -1,16 +1,17 @@
 import java.util.Scanner;
 
-class P754Converter{
+class Program{
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         int userChoice;
+        String number, convertedNumber;
         
         do{
-            System.out.println("1 - Decimal to P754");
-            System.out.println("2 - P754 to Decimal");
+            System.out.println("1 - Decimal to P754 (single precision)");
+            System.out.println("2 - P754 to Decimal (single precision)");
             userChoice = input.nextInt();
 
-            switch(choice){
+            switch(userChoice){
                 case 1:
                     break;
                 case 2: 
@@ -23,11 +24,23 @@ class P754Converter{
         }while(userChoice != 0);
     }
 
-    public static String DecimalToP754(String number){
-        //TODO
+    
+}
+
+class P754Converter{
+    public static String decimalToP754(String number){
+        int signBit;
+        
+        if(number.charAt(0) == '-') //sign bit. 1 if negative, 0 if positive.
+            signBit = 1; 
+        else
+            signBit = 0;
+
+        return null;
     }
 
     public static String P754ToDecimal(String number){
         //TODO
+        return null;
     }
 }
