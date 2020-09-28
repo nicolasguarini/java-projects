97a838fa11dcd0f1191e7ca5ea825b52ec2b1ddc
Nicolas Guarini <nikiguarincia@gmail.com>
2020-09-28 12:15:24 +0200

    Menu setup

---
+++ P754Converter.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class P754Converter{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("1 - Decimal to P754");
+        System.out.println("2 - P754 to Decimal");
+        int choice = input.nextInt();
+
+        switch(choice){
+            case 1:
+                break;
+            case 2: 
+                break;
+            case 0:
+                break;
+            default: 
+                break;
+        }
+    }
+}
